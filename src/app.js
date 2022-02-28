876eeaef9ec0769a854bfd25d7594a0601d7ce79
Marcelo Ribeiro da Silva <marcelosilva@denox.com.br>
2022-02-28 14:41:40 -0300

    config keycloak

---
+++ src/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+const keycloak = require('./config/keycloak-config.js').initKeycloak();
+app.use(keycloak.middleware());
+
+const testController = require('./controller/test-controller.js');
+app.use('/test', testController);
+
+app.get('/', function(req, res){
+   res.send("Server is up!");
+});
+
+app.listen(3000, function () {
+  console.log('Started at port 3000');
+});
---
+++ src/config/keycloak-config.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+var session = require('express-session');
+var Keycloak = require('keycloak-connect');
+
+let _keycloak;
+
+var keycloakConfig = {
+    clientId: process.env.KEYCLOACK_CLIENT_ID,
+    bearerOnly: process.env.KEYCLOACK_BEARE_ONLY,
+    serverUrl: process.env.KEYCLOACK_SERVER_URL,
+    realm: process.env.KEYCLOACK_REALM,
+    realmPublicKey: process.env.KEYCLOACK_PUBLIC_KEY
+};
+
+function initKeycloak() {
+    if (_keycloak) {
+        console.warn("Trying to init Keycloak again!");
+        return _keycloak;
+    }
+    else {
+        console.log("Initializing Keycloak...");
+        var memoryStore = new session.MemoryStore();
+        _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+        return _keycloak;
+    }
+}
+
+function getKeycloak() {
+    if (!_keycloak){
+        console.error('Keycloak has not been initialized. Please called init first.');
+    }
+    return _keycloak;
+}
+
+module.exports = {
+    initKeycloak,
+    getKeycloak
+};
